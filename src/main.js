96f4ba9bf0b6fcfaa01d5f692df205addea1e994
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-11-12 21:15:12 -0500

    sphere shape created
    sphere create and applied a placeholder texture on it.

--- src/main.js
+++ src/main.js
@@ -2,24 +2,37 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const textureLoader = new THREE.TextureLoader();
+const earthTexture = textureLoader.load('assets/earthdark.jpg');
 
-camera.position.z = 5;
+const sphereGeometrygeometry = new THREE.SphereGeometry( 3, 32, 32 );
+const sphereMaterial = new THREE.MeshBasicMaterial( { map: earthTexture } );
+const globe = new THREE.Mesh( sphereGeometrygeometry, sphereMaterial );
+
+scene.add( globe);
+
+
+
+camera.position.z = 7;
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  
+	globe.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 
 }
+
+// handle any window screen that is resizing
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
