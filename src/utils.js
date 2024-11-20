b1a99a51b3bd35474568bf5a5b23c2e3ac585f28
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-11-20 18:04:17 -0500

    added files
    need to finish map marker and data collection. making sure it eventually works.

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 //this file will add a marker on the globe. While displaying the temperature with different colors
 
+import { Scene } from "three";
+
 function colorTemperature(temp) {
     if (temp < 0) return 0x1E90FF; //cold temperature
     else if (temp => 0 && temp <= 20) return 0x00FF00; // 
@@ -7,5 +9,12 @@ function colorTemperature(temp) {
 }
 
 function getMarker(temp) {
-    //const marker 
+    const markerGeometry =  new THREE.SphereGeometry(0.2, 2, 2);
+    const markerMaterial = new THREE.MeshBasicMaterial( {color: getColorFromTemperature(temp)} );
+    const marker = new THREE.Mesh(markerGeometry, markerMaterial);
+
+    const latitude = 40.7128 * (Math.PI / 100);
+    const longtitude = 74.0060 * (Math.PI / 100);
+
+    Scene.add(marker);
 }
---
+++ src/weatherdata.js
@@ -0,0 +1,4 @@
+//this fie will be where the weather data is stored and called back to the front end (globe)
+
+API_KEY = '35e137dcc022e717eb9aaefadfae9332';
+
